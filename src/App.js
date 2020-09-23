503a52d51c0c2f7ccc5a1790ab47ee3d3282cf4f
Olumayowa Taiwo <olumayoor99@gmail.com>
2020-09-23 06:31:58 +0300

    restore point

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import GlobalStyle from "./globalStyles"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Home from "./pages/Homepage/Home"
 import { Navbar } from "./components"
 
 function App() {
@@ -8,6 +9,9 @@ function App() {
 		<Router>
 			<GlobalStyle />
 			<Navbar />
+			<Switch>
+				<Route exact path='/' component={Home} />
+			</Switch>
 		</Router>
 	)
 }
---
+++ src/pages/Homepage/Home.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { InfoSection } from "../../components"
+
+const Home = () => {
+	return (
+		<>
+			<InfoSection />
+		</>
+	)
+}
+
+export default Home
